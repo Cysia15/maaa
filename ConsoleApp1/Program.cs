7a97f045aa305050983492f9d7c09a5da41126c7
Adam2P <adiasiul534@gmail.com>
2024-10-16 12:41:55 +0200

    Pętla for

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 10; i++) 
+        {
+        Console.WriteLine(i);
+        }
+    }
+}
